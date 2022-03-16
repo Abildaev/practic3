f08977606ff22ea73a8473bad873ecfb14f4c916
Abildaev Dastan <abildaev1996@gmail.com>
2022-03-16 18:38:02 +0600

    add ad

--- js/main.js
+++ js/main.js
@@ -22,11 +22,18 @@ const products = [
         name: "Samsung",
         price: 5000,
         imgUrl: "img/ap.png"
+    },
+    {
+        id: 4,
+        name: "Samsung",
+        price: 5000,
+        imgUrl: "img/ap.png"
     }
 ]
 
 const productsId = document.getElementById('products');
 const ad = document.getElementById('ad');
+
 const searchBtn = document.getElementById('search__btn');
 const searchInput = document.getElementById('search__input')
 
@@ -42,6 +49,40 @@ products.forEach(product => {
             </div>
 `
 })
+
+/// добавление рекламы
+
+const handleAd = {
+    counterElement: document.getElementById('ad__counter-count'),
+    btnElement: document.getElementById('closeBtn'),
+    showAd: function () {
+        setTimeout(function (){
+            ad.classList.add('active')
+            this.interval();
+        }.bind(this),1000);
+    },
+    interval: function (){
+        let count = 10;
+        this.setIntervalId = setInterval(function (){
+            count--
+            this.counterElement.textContent = count;
+            if (count === 0) {
+                this.counterElement.textContent = "можете закрыть рекламу"
+                clearInterval(this.setIntervalId);
+                this.btnElement.disabled = false;
+            }
+        }.bind(this),1000)
+    },
+    hideAd: function () {
+        this.btnElement.disabled = false;
+        ad.classList.remove('active');
+    }
+}
+
+////--------
+
+handleAd.showAd();
+handleAd.btnElement.addEventListener('click', handleAd.hideAd);
 
 
 
